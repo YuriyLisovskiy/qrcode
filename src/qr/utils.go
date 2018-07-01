c9382fc7da012b8c395b0fc94958c6527e107b58
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-01 22:33:50 +0300

    small fix

--- src/qr/utils.go
+++ src/qr/utils.go
@@ -3,19 +3,3 @@ package qr
 func xor(x, y bool) bool {
 	return (x && !y) || (!x && y)
 }
-
-func makePixel(size int, px string) []string {
-	if size < 1 {
-		panic("invalid pixel size")
-	}
-	for i := 1; i < size; i++ {
-		px += px
-	}
-	res := []string{px}
-	if size > 1 {
-		for i := 1; i < size; i++ {
-			res = append(res, px)
-		}
-	}
-	return res
-}
